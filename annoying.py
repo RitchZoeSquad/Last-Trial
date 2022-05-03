e2c9acd8693864e63daeb1f5b3590db5249b742b
Your Name <ritch_cuvier@knights.ucf.edu>
2022-05-03 15:55:29 -0400

    init

---
+++ annoying.py
@@ -0,0 +1,10 @@
+
+import requests
+ 
+# Making a get request
+response = requests.get('https://api.github.com')
+ 
+# printing request content
+print(response.content)
+
+print (response.status_code)
