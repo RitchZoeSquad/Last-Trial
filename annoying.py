4f445e5d333fb7c18185eac7940adb63de81143c
RITCH CUVIER <RITCH_CUVIER@KNIGHTS.UCF.EDU>
2025-10-30 13:42:37 -0400

    Initial commit: Last-Trial (Experiment-Learning)

---
+++ annoying.py
@@ -0,0 +1,14 @@
+
+import requests
+ 
+# Making a get request
+response = requests.get('https://api.github.com')
+ 
+# printing request content
+print(response.content)
+
+print (response.status_code)
+
+I will continue Testing
+
+HAHAHA THIS IS SHITT
